9a5aa6b21ea420eb80c43ad004b52f590c447390
PhilJay Igtanloc <techpulis2021.04@gmail.com>
2024-10-26 17:10:41 +0800

    Add to cart and remove to cart

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,3 +1,4 @@
+import Button from 'react-bootstrap/Button';
 import { Card } from "react-bootstrap";
 import { formatCurrency } from "../utilities/formatCurrency";
 
@@ -9,6 +10,7 @@ type StoreItemProps = {
 };
 
 export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
+  const quantity = 0
   return (
     <Card className="h-100">
       <Card.Img
@@ -23,7 +25,23 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
           <span className="ms-2 text-muted">{formatCurrency(price)}</span>
         </Card.Title>
         <div className="mt-auto">
-          {quantiy === 0 ? <button>Add To Cart</button> : null}
+          {quantity === 0 ? (
+            <Button className='w-100'>Add To Cart</Button>
+          ) : (
+            <div
+              className="d-flex align-items-center flex-column"
+              style={{ gap: ".5rem" }}>
+                <div
+                    className="d-flex align-items-center justify-content-center"
+                    style={{ gap: ".5rem" }}>
+                    <Button>-</Button>
+                        <div>
+                            <span className="fs-3">{quantity}</span> in cart
+                        </div>
+                        <Button>+</Button>
+              </div>
+                <Button variant='danger'>Remove</Button>
+            </div> )}
         </div>
       </Card.Body>
     </Card>
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,14 +1,18 @@
 import { Col, Row } from "react-bootstrap"
+import storeItems from "../data/items.json"
 import { StoreItem } from "../components/StoreItem"
 
 export function Store() {
-    return <>
-    <h1>Store</h1>
-    <Row xs={1} md={2} lg={3} className="g-4">
-        {StoreItem.map(item => {
-            <Col key={item.id}><StoreItem {...item}/></Col>
-        })}
-    </Row>
+  return (
+    <>
+      <h1>Store</h1>
+      <Row md={2} xs={1} lg={3} className="g-4">
+        {storeItems.map(item => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
     </>
-
+  )
 }
