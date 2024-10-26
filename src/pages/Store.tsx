06f36f1e7c9c80df239a361391dbe94720c88947
PhilJay Igtanloc <techpulis2021.04@gmail.com>
2024-10-26 15:55:20 +0800

    Creating store page

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,14 @@
+import { Col, Row } from "react-bootstrap"
+import { StoreItem } from "../components/StoreItem"
+
 export function Store() {
-    return <h1>Store</h1>
+    return <>
+    <h1>Store</h1>
+    <Row xs={1} md={2} lg={3} className="g-4">
+        {StoreItem.map(item => {
+            <Col key={item.id}><StoreItem {...item}/></Col>
+        })}
+    </Row>
+    </>
+
 }
